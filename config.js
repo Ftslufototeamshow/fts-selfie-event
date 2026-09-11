f7d042751ea1e36c112ef3e4210ae15001c5924a
Ftslufototeamshow <info@fts.lu>
2026-09-11 19:22:28 +0200

    Add files via upload
    FTS Selfie Upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Diese beiden Werte aus Supabase eintragen.
+// WICHTIG: NUR den "anon" / "publishable" Key verwenden.
+// NIEMALS den service_role / secret Key hier eintragen.
+
+const FTS_SUPABASE_URL = "HIER_SUPABASE_PROJECT_URL_EINTRAGEN";
+const FTS_SUPABASE_ANON_KEY = "HIER_SUPABASE_ANON_KEY_EINTRAGEN";
