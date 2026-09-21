bc5bd7a1742af1211d10f1ddd80439ed548564f6
Ftslufototeamshow <info@fts.lu>
2026-09-21 09:43:17 +0200

    Refresh cache for customer portal ad slideshow

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('./config.js');
 const cfg=self.FTS_CONFIG;
-const CACHE='fts-selfie-v33-day-downloads-only-20260921';
+const CACHE='fts-selfie-v34-customer-ads-20260921';
 const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./offline.html','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
