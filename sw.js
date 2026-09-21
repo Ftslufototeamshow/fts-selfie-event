700f0febaab06629147fb2a8428cb72d172a7e33
Ftslufototeamshow <info@fts.lu>
2026-09-21 09:26:17 +0200

    Refresh cache for per-day ZIP and customer gallery

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('./config.js');
 const cfg=self.FTS_CONFIG;
-const CACHE='fts-selfie-v31-gallery-day-carousel-20260921';
+const CACHE='fts-selfie-v32-day-zips-customer-gallery-20260921';
 const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./offline.html','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
