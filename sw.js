7710b48c163a369e964bd85b51afd07cdec36ee2
Ftslufototeamshow <info@fts.lu>
2026-09-21 09:02:12 +0200

    Refresh cache for collapsible event studio

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('./config.js');
 const cfg=self.FTS_CONFIG;
-const CACHE='fts-selfie-v28-compact-stats-20260921';
+const CACHE='fts-selfie-v29-event-studio-accordion-20260921';
 const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./offline.html','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
