837ec850a3175050eed46488420bd4a3c5906f0e
Ftslufototeamshow <info@fts.lu>
2026-09-21 09:20:30 +0200

    FTS Galerie v31 day carousels
    Groups internal photos by event day, separates originals/designs into horizontal swipe lanes, isolates test photos, and keeps existing management actions.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('./config.js');
 const cfg=self.FTS_CONFIG;
-const CACHE='fts-selfie-v30-stats-visual-20260921';
+const CACHE='fts-selfie-v31-gallery-day-carousel-20260921';
 const CORE=['./','./index.html','./config.js','./manifest.webmanifest','./offline.html','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
